17aea2a70565e0d3490c374c568f9e6174b800cf
Zaza28 <zainebharzi26@gmail.com>
2024-05-02 01:53:49 +0200

    add all modif code home jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-
-
+import Image1 from "../images/IMG.jpg";
+import "../assets/scss/Home.scss";
 
 export default function Home(){
  return(
-   <><div>
+   <>
    <Header />
-</div><div>
-   </div> 
-    <Footer />
-    </>
+   <main>
+     <div id="banner">
+     <h1 className="banner-title">Chez vous, partout et ailleurs</h1>
+       <img className="img-banner" src={Image1} alt="images accueil" />
+     </div>
+     <section>
+       {/* Contenu de la section */}
+     </section>
+   </main>
+   <Footer />
+ </>
+
  )
 
 }
