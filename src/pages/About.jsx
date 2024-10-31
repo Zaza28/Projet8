e2ca50062da5073c7738272321b79ebcae805b5a
Zaza28 <zainebharzi26@gmail.com>
2024-10-31 21:58:19 +0100

    add all

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,10 +14,13 @@ export default function About() {
     "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme",
     "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
   ]
+
+
   return (
   <main>
    
       <Banner  cover={Image2}/>
+      
 
 
       <section className="collapse-container2">
